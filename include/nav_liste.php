5b4b6daa5c48eb3d658609eaa543cb25c70f8aab
[josubulus] <[damien.cambus@gmail.com]>
2019-01-25 03:41:22 +0100

    pagination ok a cleaner

--- include/nav_liste.php
+++ include/nav_liste.php
@@ -1,3 +1,3 @@
-<a href="index.php?idNote=<?php echo $note['id']; ?>">modifier</a>
-<a href="index.php?idNote=<?php echo $note['id']; ?>&amp;newTodo=1">+Todo</a>
-<a href="index.php">retour</a>
+<a href="index.php?idNote=<?php echo $note['id']; ?>&amp;page=<?php echo $page_int; ?>">modifier</a>
+<a href="index.php?idNote=<?php echo $note['id']; ?>&amp;newTodo=1&amp;page=<?php echo $page_int; ?>">+Todo</a>
+<a href="index.php?&amp;page=<?php echo $page_int; ?>">retour</a>
--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php session_start();
   require 'class/Formulaire.php';
   /*require 'class/Todo.php';*/
+  page(1);
+  $_SESSION['page'] = $_GET['page'];// stoque le num page pour header post et garder la page mem quand modif
 
 ?>
 <!DOCTYPE html>
@@ -22,10 +24,10 @@
     <section><!--    tout les contenu stocké en js    -->
       <?php include('include/liste.php') //fonction (todoStatut) d'affichage de la liste ?>
         <div id = "listeFait">
-          <?php todoStatut(2); ?>
+          <?php todoStatut(2, $_SESSION['page']); ?>
         </div>
         <div id="listeAFaire">
-          <?php todoStatut(1); ?>
+          <?php todoStatut(1, $_SESSION['page']);?>
         </div>
         <div id = "ajout">
           <?php include('include/ajout.php'); ?>
@@ -34,6 +36,27 @@
     <section>
       <div id = "dom">
 
+      </div>
+      <div>  <!--test pagination-->
+        <?php
+        //dev pagination :
+        function page($statut){
+          $i=0;
+          include('include/login_bdd.php');
+          $req = $bdd->prepare('SELECT * FROM note WHERE id>0 and statut=:statut ORDER BY id DESC');
+          $req->execute(array(
+            'statut' => $statut
+          ));
+          while ($page = $req->fetch()) {
+            $i_div = $i%2;
+            echo ($i_div == 0)? '<a href="index.php?page=' . $i . '">page ' . $i . '</a>' : null;
+            $i++;
+          }
+        }
+
+
+
+         ?>
       </div>
     </section>
     <script src="js/app.js"></script>
