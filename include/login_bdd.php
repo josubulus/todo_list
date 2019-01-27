b39b60c8c96f16e19d5861bbe384f2b99f4d0426
damienAcer <damien.cambus@gmail.com>
2019-01-27 15:24:55 +0100

    modification de l'include bdd pour utilisation sur acer pc

--- include/login_bdd.php
+++ include/login_bdd.php
@@ -1,7 +1,7 @@
 <?php
 try
   {
-    $bdd = new PDO('mysql:host=127.0.0.1;dbname=todo_list;charset=utf8','phpmyadmin','root',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    $bdd = new PDO('mysql:host=127.0.0.1;dbname=todo_list;charset=utf8','root','root',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
   }
   catch (Exception $e)
   {
